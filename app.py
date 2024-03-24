e07c2a1813f9783a926ce0875045027e698e4a70
ZayLinAung <zay198804@gmail.com>
2024-03-23 20:54:03 -0400

    Add Database

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import sqlite3
 
 app = Flask(__name__)
+conn = sqlite3.connect("database.db")
 
+conn.execute(
+    """
+    CREATE TABLE IF NOT EXISTS User 
+    (
+        id INTEGER PRIMARY KEY,
+        username TEXT, 
+        password TEXT
+    )
+    """
+)
 
 @app.route('/')
 def hello():
+    
+    con = sqlite3.connect("database.db")
+    cur = con.cursor()
+    cur.execute(
+        "INSERT INTO User(username, password) VALUES (?, ?)", ("Zacky", "12345"),
+    )
+    con.commit()
+
     return '<h1>Hello, World!</h1>'
 
 
